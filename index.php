251e8bddf2170b8aa13e1870d8d0837d9e1165aa
Michael L Parks <github@wmi-inc.com>
2017-10-28 09:24:05 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <header>
+      <div class="header-left">
+        <h1>RecApp</h1>
+      </div>
+      <div class="header-right">
+        <nav>
+          <ul>
+            <li><a href="">home</a></li>
+            <li><a href="">about</a></li>
+            <li><a href="">contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <div class="contact-area">
+      <h1>Dashboard</h1>
+      <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus magni fugiat 
+          architecto porro atque, ea ipsam quis alias accusamus fugit eos quae voluptate est aliquam 
+          sequi dolorum iste magnam soluta!
+      </p>
+    </div>
+  </div>
+</body>
+</html>
